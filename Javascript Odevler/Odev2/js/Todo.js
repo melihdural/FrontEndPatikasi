9a387810a35f2db422b0f8c3f772c830fce896e5
Deniz ŞAHIN <93449688+melihdural@users.noreply.github.com>
2023-03-28 12:45:41 +0300

    Odev2 completed
    Todo list completed

--- Javascript Odevler/Odev2/js/Todo.js
+++ Javascript Odevler/Odev2/js/Todo.js
@@ -1,35 +1,52 @@
-// Get all elements with class="close"
+let storedElements = []
+
 let closebtns = document.getElementsByClassName("close");
-let i;
-
-// Loop through the elements, and hide the parent, when clicked on
-for (i = 0; i < closebtns.length; i++) {
-  closebtns[i].addEventListener("click", AddCloseEvent);
-}
-
-function AddCloseEvent (){
-  this.parentElement.style.display = 'none';
-    // TODO clean on local storage
-}   
-
-
 let button = document.querySelector("#addButton")
+let i;
 
 const toDoList = document.querySelector("#toDoList")
 
 button.addEventListener("click", function() {
     let toDoInputDOM = document.querySelector('#toDoItem')
-    console.log(toDoInputDOM.value)
-
     CheckInput(toDoInputDOM)
 })
+
+SetButtonsToStorage();
+
+function SetButtonsToStorage () {
+    for (i = 0; i < closebtns.length; i++) {
+        closebtns[i].addEventListener("click", AddCloseEvent);
+        storedElements.push(closebtns[i].parentElement.innerHTML)
+        localStorage.setItem('storedElements', JSON.stringify(storedElements));
+    }
+}
+
+function AddCloseEvent (){
+    let taskList = document.getElementById("toDoList") 
+    let removedChild = this.parentElement;
+    taskList.removeChild(removedChild);
+
+    storedElements.splice(storedElements.indexOf(removedChild.innerHTML),1)
+
+    localStorage.setItem('storedElements', JSON.stringify(storedElements));
+    console.log(storedElements.length)
+}   
+
+function ResetTaskList () {
+    let taskList = document.getElementById("toDoList")
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
 
 function CheckInput (task) {
     if (task.value.length > 0) {
         try {
             let liDOM = document.createElement('li');
             liDOM.className = "list-group-item";
-            liDOM.innerHTML = `${task.value}`;
+            liDOM.innerHTML = ` 
+            ${task.value}
+            `;
             
             let closeButtonDom = document.createElement(`span`)
             closeButtonDom.className = "close";
@@ -38,14 +55,24 @@ function CheckInput (task) {
 
             liDOM.append(closeButtonDom);
             toDoList.append(liDOM);
-            /* kaydedilenler.push(liDOM.innerHTML);
-            localStorage.setItem('kaydedilenler', JSON.stringify(kaydedilenler));
-            $('.success').toast('show'); */
+
+            storedElements.push(liDOM.innerHTML);
+            
+            localStorage.setItem('storedElements', JSON.stringify(storedElements));
+            $('.success').toast('show');
             task.value = "";
         }
         catch {
             console.error("Error");
         } 
-        console.log("calistim")
     }
+}
+
+function test() {
+    ResetTaskList();
+    for (let index = 0; index < storedElements.length; index++) {
+                
+        console.log(index + storedElements[index])
+    }
+    console.log(storedElements.length)
 }
